c82dabd8b2b421134805e915e1fd7d04d4b0a0bd
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-18 19:56:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+
+
+        int seri;
+        int num1 = 0;
+        int num2 = 1, num3 = 0;
+
+        System.out.print("Serideki Eleman Sayısını Giriniz :");
+        seri = input.nextInt();
+
+        for (int i = 1; i <= seri; i++) {
+            num3 = num1 + num2;
+            System.out.print(num1 + " ");
+            num1 = num2;
+            num2 = num3;
+        }
+
+
     }
 }
